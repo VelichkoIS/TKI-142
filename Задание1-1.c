8a462e1446517ddda5bb7fea0e1c81b99e2cc115
VelichkoIS <130165421+VelichkoIS@users.noreply.github.com>
2024-09-16 15:07:10 +0300

    Update Задание1-1.c

--- Задание1-1.c
+++ Задание1-1.c
@@ -1,14 +1,24 @@
 #include <stdio.h>
 #include <math.h>
+/**
+* @brief рассчитывает а по формуле 
+* @param x - значение константы х
+* @param y - значение константы y
+* @param z - значение константы z
+* @return рассчитанное значение
+*/
+double getA(const double x, const double y, const double z); 
 
 int main()
 {
-	double x, y, a, b;
-	x = 0.335;
-	y = 0.025;
-	a = 1 + x + (pow(x, 2) / 2) + (pow (x, 3)/ 3) + (pow(x, 4)/ 4);
+	double const x = 0.335, y = 0.025;
 	b = x * (sin(pow(x, 3)) + pow(cos(y), 2));
-	printf("a = %f\nb = %f", a, b);
+	printf("a = %f\nb = %f", getA(x, y, z), b);
 
 	return 0;
+}
+
+double getA(const double x, const double y, const double z)
+{
+	return 1 + x + (pow(x, 2) / 2) + (pow (x, 3)/ 3) + (pow(x, 4)/ 4);
 }
