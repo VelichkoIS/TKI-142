b404ef60e25b48f2a68f22b2ceb2cc0c86bce9af
VelichkoIS <130165421+VelichkoIS@users.noreply.github.com>
2024-10-17 19:35:40 +0300

    Update Задание1-2.c

--- Задание1-2.c
+++ Задание1-2.c
@@ -4,11 +4,30 @@
 double R;
 const double p = 3.14;
 
-int main(){
+/**
+* @brief расчитывает значение обьема шара
+*/
+double Volume();
+
+/**
+* @brief расчитывает площадь поверхности шара
+*/
+double Square();
+
+/**
+* @brief точка входа функции
+*/
+int main() {
 	printf("%s", "Пожалуйста введите радиус шара:");
 	scanf_s("%lf", &R);
-	double V = 4 / 3 * p * pow(R, 3);
-	double S = 4 * p * pow(R, 2);
-	printf("S:%f\nV:%f", S, V);
+	printf("S:%f\nV:%f", Square(), Volume());
 	return 0;
+}
+
+double Volume() {
+	return 4 / 3 * p * pow(R, 3);
+}
+
+double Square() {
+	return 4 * p * pow(R, 2);
 }
