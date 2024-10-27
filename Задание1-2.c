005337e55dbb0828b67e184260a26f66b85460ae
VelichkoIS <130165421+VelichkoIS@users.noreply.github.com>
2024-10-27 16:45:56 +0300

    Update Задание1-2.c

--- Задание1-2.c
+++ Задание1-2.c
@@ -11,16 +11,21 @@ double Volume(double R, const double p);
 */
 double Square(double R, const double p);
 
+double Enter(double R, const double p);
+
 /**
 * @brief точка входа функции
 */
 int main() {
-    double R;
-    const double p = 3.14;
-    printf("%s", "Пожалуйста введите радиус шара:");
-    scanf_s("%lf", &R);
     printf("S: %f\nV: %f", Square(R, p), Volume(R, p));
     return 0;
+}
+
+double Enter(double R, const double p) {
+double R;
+    const double p;
+    printf("%s", "Пожалуйста введите радиус шара:");
+    scanf_s("%lf", &R);
 }
 
 double Volume(double R, const double p) {
