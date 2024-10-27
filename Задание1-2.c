2a5f198b2822c9237cc4a6d240facce77a33973d
VelichkoIS <130165421+VelichkoIS@users.noreply.github.com>
2024-10-27 18:32:29 +0300

    Update Задание1-2.c

--- Задание1-2.c
+++ Задание1-2.c
@@ -1,37 +1,46 @@
+#define _USE_MATH_DEFINES
 #include <stdio.h>
 #include <math.h>
 
 /**
-* @brief расчитывает значение обьема шара
+* @brief - рассчитывает значение объема шара
+* @param R - параметр принимающий значение радиуса
 */
-double Volume(double R, const double p);
+double Volume(double R);
 
 /**
-* @brief расчитывает площадь поверхности шара
+* @brief - рассчитывает площадь поверхности шара
+* @param R - параметр принимающий значение радиуса
 */
-double Square(double R, const double p);
-
-double Enter(double R, const double p);
+double Square(double R);
 
 /**
-* @brief точка входа функции
+* @brief - спрашивает, а затем получает значение вводимое пользователем
+* @var RE - принимает значение радиуса
+*/
+double Enter();
+
+/**
+* @brief - точка входа функции
+* @var R - переменная принимает значение функции Enter
 */
 int main() {
-    printf("S: %f\nV: %f", Square(R, p), Volume(R, p));
+    double R = Enter(); 
+    printf("S: %f\nV: %f\n", Square(R), Volume(R));
     return 0;
 }
 
-double Enter(double R, const double p) {
-double R;
-    const double p;
-    printf("%s", "Пожалуйста введите радиус шара:");
-    scanf_s("%lf", &R);
+double Enter() {
+    double RE;
+    printf("Пожалуйста, введите радиус шара: ");
+    scanf_s("%lf", &RE);
+    return RE;
 }
 
-double Volume(double R, const double p) {
-    return (4.0 / 3.0) * p * pow(R, 3);
+double Volume(double R) {
+    return (4.0 / 3.0) * M_PI * pow(R, 3);
 }
 
-double Square(double R, const double p) {
-    return 4 * p * pow(R, 2);
+double Square(double R) {
+    return 4 * M_PI * pow(R, 2);
 }
