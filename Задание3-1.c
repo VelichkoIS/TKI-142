101d8c3cfce4986f85f9a8c82abc72a047989712
VelichkoIS <130165421+VelichkoIS@users.noreply.github.com>
2024-11-24 18:15:49 +0300

    Update Задание3-1.c

--- Задание3-1.c
+++ Задание3-1.c
@@ -3,39 +3,42 @@
 
 /**
 * @ breif - принимает значения
-* @ param intervalB - указатель на адрес значения начала интервала
-* @ param intervalE - указатель на адрес значения конца интервала
-* @ param step - указатель на адрес значения шага
+* @ param x - указатель на переменную хранящую введеное число
 */
-double input(double*, double*, double*);
+double input(double*);
 
+/**
+* @ breif - проверяет значение функции
+* @ param x - параметр принимающий значение переменной x введеное число
+* @ param result - параметр принимающий значение функции для его проверки
+*/
+double check(double, double);
 
 /**
 * @ breif - точка входа в функцию
-* @ var intervalB - переменная начала интервала
-* @ var intervalE - переменная конец интервала
-* @ var step - переменная шага
 * @ var x - переменная дублируящая актуальное значение в цикле
 * @ var result - переменная результата одного шага цикла
 */
 int main(void) {
-	double intervalB, intervalE, step, x, result;
+	double x, result;
 
-	input(&intervalB, &intervalE, &step);
-	for (double i = intervalB; i < intervalE; i += step) {
-		x = i;
-		result = x + pow(x, 1 / 2) + pow(x, 1 / 3) - 2.5;
-
-			if ((isnan(result) == 1) || (isinf(result) == 1))
-				 printf("Для данного значения x = %x решения не существует");
-			else printf("%lf", result);
-	}
-
+	input(&x);
+	result = x + pow(x, 1 / 2) + pow(x, 1 / 3) - 2.5;
+	check(result, x);
+	return 0;
 }
 
-double input(double* intervalB, double* intervalE, double* step) {
-	printf("Введите интервал(два числа через двоеточие):");
-	scanf_s("%lf: %lf", intervalB, intervalE);
-	printf("Введите шаг для этого интервала:");
-	scanf_s("%lf", step);
+double input(double* x) {
+	printf("Введите число: ");
+	double c = scanf_s("%lf", x);
+	if (c != 1 || (*x) < 0.4 || (*x) > 1) {
+		printf("Введенное значение не соответсвуют указаному в задание интервалу, или не число");
+		exit(1);
+	}
+}
+
+double check(double result, double x) {
+	if ((isnan(result) == 1) || (isinf(result) == 1))
+		printf("Для данного значения x = %lf решения не существует:", x);
+	else printf("%lf", result);
 }
