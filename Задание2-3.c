c4c960a426b1dc4232090bd9072649264ff0f8e1
VelichkoIS <130165421+VelichkoIS@users.noreply.github.com>
2024-11-24 17:15:26 +0300

    Update Задание2-3.c

--- Задание2-3.c
+++ Задание2-3.c
@@ -5,22 +5,34 @@
 * @brief - определяет какое действие произвести
 * @ param n - число над которым необходисо выполнить действие
 */
-double calc(int);
+double calc(double);
 
 /**
 * @brief - точка входа в функцию
 * @var - n1, n2, n3 вещественные числа вводимые пользователем
 */
+
+/**
+* @brief - запрашивает данные у пользователя и проверяет их на соответсвие числовым типам данных
+* @param - указатель на переменную для вводимого пользователем числа
+*/
+double input(double*, double*, double*);
+
 int main(void) {
 	double n1, n2, n3;
-	
-	scanf_s("%lf%lf%lf", &n1, &n2, &n3);
-	printf("%lf %lf %lf", calc(n1), calc(n2), calc(n3));
-
+	input(&n1, &n2, &n3);
+	printf("%.2lf %.2lf %.2lf", calc(n1), calc(n2), calc(n3));
+	return 0;
 }
 
+double input(double* n1, double* n2, double* n3) {
+	if (scanf_s("%lf %lf %lf", n1, n2, n3) != 3) {
+		printf("Ошибка, введен неверный тип данных");
+		exit(1);
+	}
+}
 
-double calc(int n) {
+double calc(double n) {
 	if (n < 0)
 		return pow(n, 2);
 	return pow(n, 4);
