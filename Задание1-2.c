3f4c43e71884e05fde214b52bd198a72eaabdced
VelichkoIS <130165421+VelichkoIS@users.noreply.github.com>
2024-12-02 00:57:22 +0300

    Update Задание1-2.c

--- Задание1-2.c
+++ Задание1-2.c
@@ -1,14 +1,38 @@
 #include <stdio.h>
 #include <math.h>
 
+/**
+* @brief - принимает из stdin, а затем возвращает значение переменной value
+* @var value - переменная для хранения значения того что ввели в stdin
+* @var s - переменная для хранения количества символов введенных в stdin
+*/
+float input();
 
+/**
+* @brief main - точка входа в функцию
+* @var R - принимает значение радиуса вводимое пользователем
+* @var p - содержит в себе значение числа пи
+* @var V - содержит в себе значение обьема
+* @var S - содержит в себе значение площади
+* @return - возвращает значение 0 при успешном выполнении программы 
+*/
 int main(){
-	float R;
-	const double p = 3.14;
-	printf("%s", "Пожалуйста введите радиус шара:");
-	scanf_s("%lf", &R);
+	float R = input();
+	const float p = 3.14;
+	puts("Пожалуйста введите радиус шара");
 	double V = 4 / 3 * p * pow(R, 3);
 	double S = 4 * p * pow(R, 2);
 	printf("S:%f\nV:%f", S, V);
 	return 0;
+}
+
+float input() {
+	float value = 0.0;
+	int s = scanf_s("%f", &value);
+	if (s != 1) {
+		errno = EIO;
+		perror("Ошибка, не числовое значение\n");
+		exit(EXIT_FAILURE);
+	}
+	return value;
 }
