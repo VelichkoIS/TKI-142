1974ee59b232852bdd5febb0827caaafccf819e0
VelichkoIS <130165421+VelichkoIS@users.noreply.github.com>
2024-12-15 21:06:03 +0300

    Update Задание 4-1.c

--- Задание 4-1.c
+++ Задание 4-1.c
@@ -4,81 +4,81 @@
 #include <errno.h>
 
 /**
-* @brief - принимает из stdin, а затем возвращает значение переменной value
-* @var value - переменная для хранения значения того что ввели в stdin
-* @var s - переменная для хранения количества символов введенных в stdin
+* @brief принимает из stdin, а затем возвращает значение переменной value
+* @return числовое значение полученное из stdin
 */
-int input();
+int input(void);
 
 /**
-* @brief - принимает из stdin, а затем возвращает значение переменной value, проверяет является ли число положительным
-* @var value - переменная для хранения значения того что ввели в stdin
-* @var s - переменная для хранения количества символов введенных в stdin
+* @brief принимает из stdin, а затем возвращает значение переменной value, проверяет является ли число положительным
+* @return положительное числовое значение
 */
-size_t input_p();
+size_t input_positive(void);
 
 /**
-* @brief - проверяет размер массива
-* @param size - размер массива
+* @brief заполняет массив случайно сгенерироваными числами
+* @param array указатель на массив
+* @param size размер массива
 */
-size_t check_size(const size_t size);
+void random_input(int* const array, const size_t size);
 
 /**
-* @brief - заполняет массив случайно сгенерироваными числами
-* @param array - содержимое массива
-* @param size - размер массива
+* @brief заполняет массив из стандартного потока ввода
+* @param array указатель на массив
+* @param size размер массива
 */
-int r_input(int* array, const size_t size);
+void keyboard_input(int* array, const size_t size);
 
 /**
-* @brief - заполняет массив из стандартного потока ввода
-* @param array - содержимое массива
-* @param size - размер массива
+* @brief расчитывает сумму элементов, имеющих нечетные индексы.
+* @param array указатель на массив
+* @param size размер массива
+* @return сумму элементов, имеющих нечетные индексы
 */
-int k_input(int* array, const size_t size);
+int sum(const size_t size, int* const array);
 
 /**
-* @brief - Расчитывает сумму элементов, имеющих нечетные индексы.
-* @param array - содержимое массива
-* @param size - размер массива
+* @brief расчитывает количество элементов массива, значения которых больше заданного числа А и кратных 5.
+* @param array указатель на массив
+* @param size размер массива
+* @return количество элементов массива, значения которых больше заданного числа А и кратных 5.
 */
-int sum(const size_t size, int* array);
+int count(const int A, const size_t size, int* array);
 
 /**
-* @brief - Расчитывает количество элементов массива, значения которых больше заданного числа А и кратных 5.
-* @param array - содержимое массива
-* @param size - размер массива
-*/
-int count(const size_t size, int* array);
-
-/**
-* @brief - Делит все элементы массива с четными номерами на первый элемент (первый элемент отличен от 0).
-* @param array - содержимое массива
-* @param size - размер массива
-* @param array_float - содержимое массива чисел с плавающей точкой
+* @brief делит все элементы массива с четными номерами на первый элемент (первый элемент отличен от 0).
+* @param array указатель на массив
+* @param size размер массива
+* @param array_float указатель на массив чисел с плававющей точкой
 */
 void division(const size_t size, int* array, float* array_float);
 
 /**
-* @brief - выводит содержимое массива
-* @param array - содержимое массива
-* @param size - размер массива
+* @brief выводит содержимое массива
+* @param array указатель на массив
+* @param size размер массива
 */
 void print_array(int* array, const size_t size);
 
 /**
-* @brief - выводит содержимое массива чисел с плавающей точкой
-* @param array - содержимое массива
-* @param size - размер массива
+* @brief выводит содержимое массива чисел с плавающей точкой
+* @param array указатель на массив
+* @param size размер массива
 */
 void print_array_float(float* array_float, const size_t size);
 
 /**
-* @brief - проверяет правильность интервала
-* @param min - минимально возможное значение элемента
-* @param max - максимально возможное значение элемента
+* @brief проверяет правильность интервала
+* @param min минимально возможное значение элемента
+* @param max максимально возможное значение элемента
 */
-int check_mm(const int min, const int max);
+void check_range(const int min, const int max);
+
+/**
+* @brief проверяет выделение памяти
+* @param array указатель на массив
+*/
+void check_array(int* const array);
 
 enum method {
 	RANDOM,
@@ -87,18 +87,19 @@ enum method {
 
 int main() {
 	puts("Введите размер массива:");
-	size_t size = check_size(input_p());
+	size_t size = input_positive();
 	int* array = (int*)malloc(size * sizeof(int));
+	check_array(array);
 	puts("Выберите способ заполнения массива:\n0 - случайными числами\n1 - вручную");
-	enum method choice  = input_p();
+	enum method choice = (enum method)input_positive();
 	switch (choice) {
 	case RANDOM:
-		r_input(array, size);
+		random_input(array, size);
 		puts("Массив: ");
 		print_array(array, size);
 		break;
 	case KEYBOARD:
-		k_input(array, size);
+		keyboard_input(array, size);
 		break;
 	default:
 		free(array);
@@ -106,9 +107,12 @@ int main() {
 		perror("Введены неверные значения\n");
 		exit(1);
 	}
-	sum(size, array);
-	count(size, array);
+	printf("Сумма всех элементов с нечетными индексами: %d\n", sum(size, array));
+	printf("Введите число A (с которым будут сравниваться элементы массива): ");
+	int A = input();
+	printf("Количество элементов массива значения которых больше %d и кратны 5: %d\n", A, count(A, size, array));
 	float* array_float = (float*)malloc(size * sizeof(float));
+	check_array(array_float);
 	division(size, array, array_float);
 	free(array);
 	puts("Массив после деления всех элементов с четными номерами на первый элемент: ");
@@ -129,10 +133,10 @@ int input() {
 	return value;
 }
 
-size_t input_p() {
-	int value = 0.0;
-	int s = scanf_s("%d", &value);
-	if (s != 1 || s < 0) {
+size_t input_positive() {
+	size_t value = 0.0;
+	int s = input();
+	if (s < 0) {
 		errno = EIO;
 		perror("Ошибка, не числовое значение или число отрицательное\n");
 		exit(1);
@@ -140,57 +144,44 @@ size_t input_p() {
 	return value;
 }
 
-size_t check_size(const size_t size) {
-	if (size > 50) {
-		errno = EIO;
-		perror("Указано слишком большое число элементов массива\n");
-		exit(1);
-	}
-	return size;
-}
-
-int r_input(int* array, const size_t size) {
+void random_input(int* const array, const size_t size) {
 	srand(time(NULL));
 	puts("Введите минимальное значение элемента массива:");
 	int min = input();
 	puts("Введите максимальное значение элемента массива:");
 	int max = input();
-	check_mm(min, max);
-	for (int i = 0; i < size; i++) {
+	check_range(min, max);
+	for (size_t i = 0; i < size; i++) {
 		array[i] = rand() % (max - min + 1) + min;
 	}
-	return 0;
 }
 
-int k_input(int* array, const size_t size) {
+void keyboard_input(int* array, const size_t size) {
 	for (int i = 0; i < size; i++) {
 		array[i] = input();
 	}
 }
 
-int sum(const size_t size, int* array) {
+int sum(const size_t size, int* const array) {
 	int temp = 0;
 	for (int i = 1; i < size; i += 2) {
 		temp += array[i];
 	}
-	printf("Сумма всех элементов с нечетными индексами: %d\n", temp);
 	return temp;
 }
 
-int count(const size_t size, int* array) {
-	printf("Введите число A (с которым будут сравниваться элементы массива): ");
-	int A = input();
+int count(const int A, const size_t size, int* array) {
+	printf("Введите число A (с которым будут сравниваться элементы массива): ");;
 	int c = 0;
 	for (int i = 0; i < size; i++) {
 		if ((array[i] > A) && (array[i] % 5 == 0)) {
 			c++;
 		}
 	}
-	printf("Количество элементов массива значения которых больше %d и кратны 5: %d\n", A, c);
 	return c;
 }
 
-size_t division(const size_t size, int* array, float* array_float) {
+void division(const size_t size, int* array, float* array_float) {
 	for (int i = 0; i < size; i++) {
 		if (i % 2 == 0) {
 			array_float[i] = (float)array[i] / (float)array[0];
@@ -198,7 +189,6 @@ size_t division(const size_t size, int* array, float* array_float) {
 		else
 			array_float[i] = array[i];
 	}
-	return array_float;
 }
 
 void print_array(int* array, const size_t size) {
@@ -213,11 +203,17 @@ void print_array_float(float* array_float, const size_t size) {
 	}
 }
 
-int check_mm(const int min, const int max) {
+void check_range(const int min, const int max) {
 	if (min > max) {
 		errno = EIO;
 		perror("Не верно указан интервал\n");
 		exit(1);
 	}
-	return 0;
+}
+
+void check_array(int* const array) {
+	if (!array) {
+		perror("Ошибка: массив не выделен");
+		exit(EXIT_FAILURE);
+	}
 }
