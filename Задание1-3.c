dc5da2498b1f20c0aabf7e9ca8c9e27746a8e7fd
VelichkoIS <130165421+VelichkoIS@users.noreply.github.com>
2024-12-21 20:46:41 +0300

    Update Задание1-3.c

--- Задание1-3.c
+++ Задание1-3.c
@@ -7,22 +7,25 @@
 * @brief принимает числовое значение из stdin
 * @return числовое значение полученное из stdin
 */
-void input(void);
+double input(void);
 
 /**
 * @brief расчитывает сопротивление
 * @param R1, R2, R3 - пременные
 * @return численное значение сопротивления
 */
-double resistance(double R1, double R2, double R3);
+double get_r(const double R1, const double R2, const double R3);
 
 /**
 * @brief точка входа функции
 * @return 0 в случае успешного выполнения программы
 */
 int main(void) {
+	puts("Введите сопротивление первого резистора:");
     double R1 = input();
+	puts("Введите сопротивление второго резистора:");
     double R2 = input();
+	puts("Введите сопротивление третьего резистора:");
     double R3 = input();
     printf("R: %lf\n", get_r(R1, R2, R3));
     return 0;
@@ -39,6 +42,6 @@ double input(void) {
 	return value;
 }
 
-double get_r(double R1, double R2, double R3) {
-    return (R1 * R2 * R3) / (R1 * R2 + R2 * R3 + R3 * R1);
+double get_r(const double R1, const double R2, const double R3) {
+    return (R1 * R2 * R3) / (R1 * R2 + R1 * R3 + R2 * R3);
 }
