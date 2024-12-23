d551b1d294402431eacb4e1dc9fddf008dc33ed4
VelichkoIS <130165421+VelichkoIS@users.noreply.github.com>
2024-12-23 09:07:59 +0300

    Create main.c

---
+++ Задание4-2/main.c
@@ -0,0 +1,41 @@
+#include "myheader.h"
+
+int main() {
+	puts("Введите размер массива:");
+	size_t size = input_positive();
+	int* array = (int*)malloc(size * sizeof(int));
+	check_array(array);
+	printf("Выберите способ заполнения массива:\n%d - случайными числами\n%d - вводом с клавиатуры\n", (int)random, (int)keyboard);
+	enum method choice = (enum method)input_positive();
+	switch (choice) {
+	case random:
+		random_input(array, size);
+		puts("Массив: ");
+		print_array(array, size);
+		break;
+	case keyboard:
+		keyboard_input(array, size);
+		break;
+	default:
+		free(array);
+		errno = EIO;
+		perror("Введены неверные значения\n");
+		exit(1);
+	}
+	change(array, size);
+	delete_elements(array, size);
+
+
+	int* new_array = (int*)malloc(size * sizeof(int)); ;
+	check_array(new_array);
+
+	create_array(array, new_array, size);
+
+	puts("\nПолученный массив A: \n");
+	print_array(new_array, size);
+
+	free(array);
+	free(new_array);
+
+	return 0;
+}
