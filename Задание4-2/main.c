2ed36ed1a4e42649cde3657e0f67e9d4509ca913
VelichkoIS <130165421+VelichkoIS@users.noreply.github.com>
2024-12-23 14:39:16 +0300

    Update main.c

--- Задание4-2/main.c
+++ Задание4-2/main.c
@@ -3,7 +3,7 @@
 int main() {
 	puts("Введите размер массива:");
 	size_t size = input_positive();
-	create_array(size);
+	int* array = create_array(size);
 	printf("Выберите способ заполнения массива:\n%d - случайными числами\n%d - вводом с клавиатуры\n", (int)random, (int)keyboard);
 	enum method choice = (enum method)input_positive();
 	switch (choice) {
@@ -23,7 +23,7 @@ int main() {
 	}
 	change(array, size);
 	delete_elements(array, size);
-	create_array(size);
+	int* new_array = create_array(size);
 	array_filling(array, new_array, size);
 	puts("\nПолученный массив A: \n");
 	print_array(new_array, size);
