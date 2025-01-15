71e1023fb8c1f789c41c285e3652a71a3675e661
VelichkoIS <130165421+VelichkoIS@users.noreply.github.com>
2025-01-15 22:16:50 +0300

    Update Задание1-1.c

--- Задание1-1.c
+++ Задание1-1.c
@@ -6,7 +6,7 @@
 * @return рассчитанное значение
 * @param x - константа x
 */
-double getA(const double x);
+double get_a(const double x);
 
 /*
 * @brief рассчитывает значения b по формуле
@@ -14,28 +14,31 @@ double getA(const double x);
 * @param x - константа x
 * @param y - константа y
 */
-double getB(const double x, const double y);
+double get_b(const double x, const double y);
 
 /*
 * @brief - точка входа функции
-* @return - возвращает 0 в случае успешного выполнения программы 
+* @return - возвращает 0 в случае успешного выполнения программы
 * @param x - константа x
 * @param y - константа y
 */
 int main()
 {
-	double const x = 0.335, y = 0.025;
-	printf("a = %f\nb = %f", getA(x), getB(x, y));
+	double const x = 0.335;
+	double const y = 0.025;
+	double const a = get_a(x);
+	double const b = get_b(x, y);
+	printf("a = %lf\nb = %lf", a, b);
 
 	return 0;
 }
 
-double getA(const double x)
+double get_a(const double x)
 {
 	return 1 + x + (pow(x, 2) / 2) + (pow(x, 3) / 3) + (pow(x, 4) / 4);
 }
 
-double getB(const double x, const double y)
+double get_b(const double x, const double y)
 {
 	return x * (sin(pow(x, 3)) + pow(cos(y), 2));
 }
